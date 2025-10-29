ce55d6bf6be019f17e097257254c7a419891162b
wuritz <ritzrichard51@gmail.com>
2025-10-29 19:13:43 +0100

    footer + scroll to top

---
+++ components/Helper/ScrollToTop.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, {useEffect} from 'react'
+import {FaArrowUp} from "react-icons/fa";
+
+const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 300) setIsVisible(true);
+            else setIsVisible(false);
+        };
+
+        window.addEventListener('scroll', toggleVisibility);
+        return () => window.removeEventListener('scroll', toggleVisibility);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    return (
+        <div className={"fixed bottom-4 animate-pulse right-4"}>
+            {isVisible && (
+                <button className={"bg-blue-950/60 cursor-pointer text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none"}
+                onClick={scrollToTop}>
+                    <FaArrowUp />
+                </button>
+            )}
+        </div>
+    )
+}
+export default ScrollToTop
---
+++ components/Home/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className={"bg-blue-900/20 p-5"}>
+            <h1 className={"text-lg text-white/70 text-center"}>
+                Genyo fasz fasz fasz fasz
+            </h1>
+        </div>
+    )
+}
+export default Footer

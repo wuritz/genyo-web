2eea05ad0a3fca6428857f6db5dd662d1cd0f1f3
wuritz <ritzrichard51@gmail.com>
2025-10-29 09:30:54 +0100

    started

---
+++ components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="overflow-hidden h-[2000px]">Home</div>
+    )
+}
+export default Home
---
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from "@/components/Home/Navbar/Nav";
+import MobileNav from "@/components/Home/Navbar/MobileNav";
+
+const ResponsiveNav = () => {
+    return (
+        <div>
+            <Nav />
+            <MobileNav />
+        </div>
+    )
+}
+export default ResponsiveNav

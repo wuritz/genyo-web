4c3016d43cc3e65e274e969faa55cab715a3cd96
wuritz <ritzrichard51@gmail.com>
2025-10-29 18:12:51 +0100

    skills are now done hahahahahahahahhahaha

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from 'react'
 import {SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript} from "react-icons/si";
+import Tilt from "react-parallax-tilt";
 
 const skills = [
     {
@@ -40,6 +42,20 @@ const Skills = () => {
             <h1 className={"text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white"}>
                 My <span className={"text-cyan-300"}>Skills</span>
             </h1>
+
+            <div className={"flex flex-wrap justify-center gap-6 mt-16"}>
+                {skills.map((skill) => {
+                    return <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+                        <div className={"bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105"}>
+                            <div className={"text-5xl mb-4 text-gray-300"}>
+                                {skill.icon}
+                            </div>
+                            <p className={"text-2xl font-semibold"}>{skill.percentage}%</p>
+                            <p className={"text-purple-400 mt-1"}>{skill.name}</p>
+                        </div>
+                    </Tilt>
+                })}
+            </div>
         </div>
     )
 }

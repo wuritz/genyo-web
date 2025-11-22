6214b27db09a265023b16f584789d61efe6f4b1f
wuritz <ritzrichard51@gmail.com>
2025-11-22 21:40:49 +0100

    really start to rewrite

---
+++ app/faq/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FaqSec from "@/components/FAQ/faq-sec";
+import DownloadSec from "@/components/Home/download-sec";
+
+const FaqPage = () => {
+    return (
+        <div className={"min-h-screen bg-white text-black scroll-smooth"}>
+            <section className={"relative flex flex-col"}>
+                <FaqSec />
+                <DownloadSec />
+            </section>
+        </div>
+    )
+}
+export default FaqPage
--- components/Home/navbar/navbar-sec.tsx
+++ components/Home/navbar/navbar-sec.tsx
@@ -19,10 +19,14 @@ const NavbarSec = () => {
     return (
         <nav className={`border-b-4 ${invert ? "backdrop-invert backdrop-blur-sm" : ""} border-black p-6 fixed w-full z-[1000] transition-all duration-300`}>
             <div className="max-w-7xl mx-auto flex justify-between items-center">
-                <div className={`tracking-tight ${invert ? "text-white bg-black" : "text-black"} `}>Genyo</div>
+                <div className={`tracking-tight ${invert ? "text-white bg-black" : "text-black"} `}>
+                    <a href={"/"}>
+                        Genyo
+                    </a>
+                </div>
                 <div className="flex gap-4">
-                    <NavbarButton invert={invert} text={"FAQ"} link={"/faq"} />
-                    <NavbarButton invert={invert} text={"GITHUB"} link={"https://github.com/wuritz/genyo-addon"} />
+                    <NavbarButton invert={invert} text={"FAQ"} link={"/faq"} target={false} />
+                    <NavbarButton invert={invert} text={"GITHUB"} link={"https://github.com/wuritz/genyo-addon"} target={true}/>
                 </div>
             </div>
         </nav>
